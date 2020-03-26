2ce2e327875b8b661e133a162d8e99240f5b4e57
tonnon <lucastonnon@gmail.com>
2020-03-26 05:31:45 -0300

    vai

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -41,7 +41,7 @@ export default function Register(){
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Voltar para o Login
+                        Voltar para o login
                     </Link>
                 </section>
                 <form onSubmit={handleRegister}>
